129a9e9c5cce59bb75b55971d08fc01fef2de5ac
zhangbin <bg@bmmgo.com>
2018-10-10 17:41:54 +0800

    package sql generator

---
+++ csharp/im.sdk/SeqGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+namespace im.sdk
+{
+    public sealed class SeqGenerator
+    {
+        private readonly ConcurrentQueue<int> _seqPool;
+        private int _seed;
+        public static readonly SeqGenerator Instance = new SeqGenerator();
+
+        private SeqGenerator()
+        {
+            _seqPool = new ConcurrentQueue<int>();
+        }
+
+        public int GetSeq()
+        {
+            if (_seqPool.TryDequeue(out int seq))
+            {
+                return seq;
+            }
+            return Interlocked.Increment(ref _seed);
+        }
+
+        public void FreeSeq(int seq)
+        {
+            _seqPool.Enqueue(seq);
+        }
+    }
+}
